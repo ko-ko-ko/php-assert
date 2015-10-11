db9240c3657b81e97b3b514cafc5f291fc07922e
Roman Levishchenko <index.0h@gmail.com>
2015-10-12 02:49:04 +0300

    add comments

--- src/exceptions/InvalidLengthException.php
+++ src/exceptions/InvalidLengthException.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link      https://github.com/ko-ko-ko/php-assert
+ * @copyright Copyright (c) 2015 Roman Levishchenko <index.0h@gmail.com>
+ * @license   https://raw.github.com/ko-ko-ko/php-assert/master/LICENSE
+ */
 
 namespace KoKoKo\assert\exceptions;
 
